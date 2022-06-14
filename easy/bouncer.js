1223044eafd385c1f883a65a84db7641decce1c4
Pam Whisenhunt <pam.s.whisenhunt@gmail.com>
2022-06-14 12:04:34 -0500

    bouncer

--- easy/bouncer.js
+++ easy/bouncer.js
@@ -5,11 +5,28 @@
  */
 
 // 1,236,830 ops/s
-const bouncer = (arr) => {
+const bouncerV1 = (arr) => {
     return arr.filter((item) => item)
 }
 
-console.log(bouncer([7, "ate", "", false, 9])) // [7, 'ate', 9]
-console.log(bouncer([false, null, 0, NaN, undefined, ""])) // []
-console.log(bouncer(["a", "b", "c"])) // ['a', 'b', 'c']
-console.log(bouncer([null, NaN, 1, 2, undefined])) // [1,2]
+console.log(bouncerV1([7, "ate", "", false, 9])) // [7, 'ate', 9]
+console.log(bouncerV1([false, null, 0, NaN, undefined, ""])) // []
+console.log(bouncerV1(["a", "b", "c"])) // ['a', 'b', 'c']
+console.log(bouncerV1([null, NaN, 1, 2, undefined])) // [1,2]
+
+
+// 1,014,610 ops/s
+const bouncerV2 = (arr) => {
+    let truthyArr = []
+    arr.forEach((item) => {
+        if (item) {
+            truthyArr.push(item)
+        }
+    })
+    return truthyArr
+}
+
+console.log(bouncerV2([7, "ate", "", false, 9])) // [7, 'ate', 9]
+console.log(bouncerV2([false, null, 0, NaN, undefined, ""])) // []
+console.log(bouncerV2(["a", "b", "c"])) // ['a', 'b', 'c']
+console.log(bouncerV2([null, NaN, 1, 2, undefined])) // [1,2]
